50812a29f5c4ca495ddb42cf0f7fdf9dca336644
umendibe <ikumendibe25@lhusurbil.eus>
2026-09-24 10:59:53 +0200

    mendiak-component terminado

---
+++ src/app/tarifak-component/tarifak-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-tarifak-component',
+  styleUrl: './tarifak-component.css',
+  templateUrl: './tarifak-component.html',
+})
+export class TarifakComponent {}
